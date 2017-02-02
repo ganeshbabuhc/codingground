981dc41afa66d8dd2309c96f631b83e25f725c10
Ganeshbabu chowdareddy <ganesh99454@gmail.com>
2017-02-02 06:50:02 +0530

    Created by Coding Ground Client

---
+++ New Project-20170202/main.php
@@ -0,0 +1,105 @@
+<?php
+require 'Newfile.php';
+$driver = 'mysql';
+$database = "dbname=CODINGGROUND";
+$dsn = "$driver:host=localhost;unix_socket=/home/cg/mysql/mysql.sock;$database";
+
+$username = 'root';
+$password = 'root';
+
+// try {
+//   $conn = new PDO($dsn, $username, $password);
+//   echo "<h2>Database CODINGGROUND Connected<h2>";
+// }catch(PDOException $e){
+//   echo "<h1>" . $e->getMessage() . "</h1>";
+// }
+// $sql = 'SELECT  * FROM users';
+// $stmt = $conn->prepare($sql);
+// $stmt->execute();
+
+// echo "<table style='width:100%'>";
+// while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+//   echo "<tr>";
+//   foreach($row as $value)
+//   {
+//     echo sprintf("<td>%s</td>", $value);
+//   }
+//   echo "</tr>";
+// }
+// echo "</table>";
+
+ function mycurl($url)
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_USERAGENT, "Googlebot/2.1 (http://www.googlebot.com/bot.html)");
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+		
+		// Gagal ngecURL
+        if(!$site = curl_exec($ch)){
+			return 'offline';
+		}
+		
+		// success ngecURL
+		else{
+			return $site;
+		}
+	}  
+
+
+$url  = 'http://www.gsmarena.com/makers.php3';
+//st-text
+		$ngecurl = mycurl($url);
+		
+		$html  = str_get_html($ngecurl);
+			$div = $html->find('div[class=st-text]', 0);
+			
+			 $index=0;
+			  foreach  ($div->find('td') as $td) {
+				     $a=$td->find('a',0);
+				    $page= $a->getAttribute('href');
+				    //page for each brand list
+				     $url="http://www.gsmarena.com/".$page;
+				    
+				     if($index<2){
+				         $ngecurl = mycurl($url);
+				     	$html  = str_get_html($ngecurl);
+				     	$div = $html->find('div[class=makers]', 0);
+				     	 $index1=1;
+				     	 //page for each mobile
+				     	   foreach  ( $div->find('li') as $li) {
+				     	       echo  $index1.$li->find('a',0)->getAttribute('href').'<br>';
+				     	        $index1++;
+				     	        //write mobile details geting code
+				     	   }
+				     	   
+				     	  // $nav= $html->find('div[class=nav-pages]', 0)->find('a');
+				     	  // if(sizeof($nav)>0)
+				     	  // {
+				     	      
+				     	  // }
+				     	  
+				     	  foreach($html->find('div[class=nav-pages]', 0)->find('a') as $aa){
+				     	     $url="http://www.gsmarena.com/".$aa->getAttribute('href');
+				     	      
+				     	      
+				     	        $ngecurl = mycurl($url);
+				     	$html  = str_get_html($ngecurl);
+				     	$div = $html->find('div[class=makers]', 0);
+				     	 
+				     	 //page for each mobile
+				     	   foreach  ( $div->find('li') as $li) {
+				     	       echo  $index1.$li->find('a',0)->getAttribute('href').'<br>';
+				     	        $index1++;
+				     	        //write mobile details geting code
+				     	   }
+				     	  }
+				     	   
+				     }
+				     $index++;
+				    
+				 }
+?>
